bee6f40331d0a53fd0841e6e043081ec31754ef2
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-05-27 22:01:52 +0300

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Vlad on 27/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
